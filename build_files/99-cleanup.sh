b79676b765223d2422067d0ca05772dad682b809
givensuman <givensuman@duck.com>
2025-12-16 11:59:54 -0500

    Revise toolbox system

--- build_files/99-cleanup.sh
+++ build_files/99-cleanup.sh
@@ -82,6 +82,55 @@ mkdir -p /tmp
 mkdir -p /var/tmp
 chmod -R 1777 /var/tmp
 
+# Disable COPRs and non-essential repos
+dnf5 -y copr disable ublue-os/staging || true
+dnf5 -y copr disable ublue-os/packages || true
+dnf5 -y copr disable phracek/PyCharm || true
+
+dnf5 config-manager setopt negativo17-fedora-multimedia.enabled=0 || true
+dnf5 config-manager setopt _copr_ublue-os-akmods.enabled=0 || true
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=0 || true
+
+dnf5 config-manager setopt terra.enabled=0 || true
+dnf5 config-manager setopt docker-ce.enabled=0 || true
+
+dnf5 config-manager setopt rpmfusion-nonfree-nvidia-driver.enabled=0 || true
+dnf5 config-manager setopt rpmfusion-nonfree-steam.enabled=0 || true
+# Or, as fallback
+for i in /etc/yum.repos.d/rpmfusion-*; do
+  sed -i 's@enabled=1@enabled=0@g' "$i"
+done
+
+# To be safe...
+repos=(
+  docker-ce
+  terra
+  fedora-cisco-openh264
+  fedora-updates
+  fedora-updates-archive
+  fedora-updates-testing
+  google-chrome
+  negativo17-fedora-multimedia
+  negativo17-fedora-nvidia
+  nvidia-container-toolkit
+  rpm-fusion-nonfree-nvidia-driver
+  rpm-fusion-nonfree-steam
+)
+
+for repo in "${repos[@]}"; do
+  if [[ -f "/etc/yum.repos.d/${repo}.repo" ]]; then
+    sed -i 's@enabled=1@enabled=0@g' "/etc/yum.repos.d/${repo}.repo"
+  fi
+done
+
+if ls /etc/yum.repos.d/_copr*.repo &>/dev/null; then
+  coprs=()
+  mapfile -t coprs <<<"$(find /etc/yum.repos.d/_copr*.repo)"
+  for copr in "${coprs[@]}"; do
+    sed -i 's@enabled=1@enabled=0@g' "$copr"
+  done
+fi
+
 # Commit and lint container
 ostree container commit
 bootc container lint
--- system_files/etc/profile.d/dune.sh
+++ system_files/etc/profile.d/dune.sh
@@ -2,17 +2,15 @@
 
 # ghostty
 if [ -n "${GHOSTTY_RESOURCES_DIR}" ]; then
-  if [ -f "${GHOSTTY_RESOURCES_DIR}/shell-integration/bash/ghostty.bash" ]; then
-    builtin source "${GHOSTTY_RESOURCES_DIR}/shell-integration/bash/ghostty.bash"
-  fi
+  builtin source "${GHOSTTY_RESOURCES_DIR}/shell-integration/bash/ghostty.bash"
 elif [ -f /usr/share/ghostty/shell-integration/bash/ghostty.bash ]; then
   builtin source /usr/share/ghostty/shell-integration/bash/ghostty.bash
 fi
 if [[ "$TERM" == "xterm-ghostty" ]]; then
-    # Check if the terminfo actually exists; if not, fallback to xterm-256color
-    if ! infocmp xterm-ghostty >/dev/null 2>&1; then
-        export TERM=xterm-256color
-    fi
+  # Check if the terminfo actually exists; if not, fallback to xterm-256color
+  if ! infocmp xterm-ghostty >/dev/null 2>&1; then
+    export TERM=xterm-256color
+  fi
 fi
 
 # Enable Flatpak exports
