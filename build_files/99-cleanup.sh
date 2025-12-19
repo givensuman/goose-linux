3f6f40a04048799877adc48c34677e10681b90b6
givensuman <givensuman@duck.com>
2025-12-18 21:55:00 -0500

    Add back ublue-brew

--- build_files/99-cleanup.sh
+++ build_files/99-cleanup.sh
@@ -51,6 +51,56 @@ for repo in /etc/yum.repos.d/_copr*.repo; do
   sed -i 's@enabled=1@enabled=0@g' "$repo"
 done
 
+# Disable COPRs and non-essential repos
+dnf5 -y copr disable ublue-os/staging || true
+dnf5 -y copr disable ublue-os/packages || true
+dnf5 -y copr disable phracek/PyCharm || true
+
+dnf5 config-manager setopt negativo17-fedora-multimedia.enabled=0 || true
+dnf5 config-manager setopt _copr_ublue-os-akmods.enabled=0 || true
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=0 || true
+
+dnf5 config-manager setopt terra.enabled=0 || true
+dnf5 config-manager setopt docker-ce.enabled=0 || true
+
+dnf5 config-manager setopt rpmfusion-nonfree-nvidia-driver.enabled=0 || true
+dnf5 config-manager setopt rpmfusion-nonfree-steam.enabled=0 || true
+
+# Or, as fallback
+[[ find /etc/yum.repos.d/rpmfusion-* ]] && {
+  for repo in /etc/yum.repos.d/rpmfusion-*; do
+    sed -i 's@enabled=1@enabled=0@g' "$repo"
+  done
+}
+
+# To be safe...
+repos=(
+  docker-ce
+  terra
+  fedora-cisco-openh264
+  fedora-updates
+  fedora-updates-archive
+  fedora-updates-testing
+  google-chrome
+  negativo17-fedora-multimedia
+  negativo17-fedora-nvidia
+  nvidia-container-toolkit
+  rpm-fusion-nonfree-nvidia-driver
+  rpm-fusion-nonfree-steam
+)
+
+for repo in "${repos[@]}"; do
+  if [[ -f "/etc/yum.repos.d/${repo}.repo" ]]; then
+    sed -i 's@enabled=1@enabled=0@g' "/etc/yum.repos.d/${repo}.repo"
+  fi
+done
+
+[[ find /etc/yum.repos.d/_copr*.repo ]] && {
+  for repo in /etc/yum.repos.d/_copr*.repo; do
+    sed -i 's@enabled=1@enabled=0@g' "$repo"
+  done
+}
+
 # Clean up temporary files and caches
 rm -rf /tmp/* || true
 rm -rf /var/tmp/* || true
@@ -81,21 +131,6 @@ done
 mkdir -p /tmp
 mkdir -p /var/tmp
 chmod -R 1777 /var/tmp
-
-# Disable COPRs and non-essential repos
-dnf5 -y copr disable ublue-os/staging || true
-dnf5 -y copr disable ublue-os/packages || true
-dnf5 -y copr disable phracek/PyCharm || true
-
-dnf5 config-manager setopt negativo17-fedora-multimedia.enabled=0 || true
-dnf5 config-manager setopt _copr_ublue-os-akmods.enabled=0 || true
-dnf5 config-manager setopt fedora-cisco-openh264.enabled=0 || true
-
-dnf5 config-manager setopt terra.enabled=0 || true
-dnf5 config-manager setopt docker-ce.enabled=0 || true
-
-dnf5 config-manager setopt rpmfusion-nonfree-nvidia-driver.enabled=0 || true
-dnf5 config-manager setopt rpmfusion-nonfree-steam.enabled=0 || true
 
 # Commit and lint container
 ostree container commit
