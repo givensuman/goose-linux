31bd136d04458b4737f42ec05ced0d70d8b69a62
givensuman <givensuman@duck.com>
2025-12-18 21:55:00 -0500

    Add back ublue-brew

--- build_files/50-ublue.sh
+++ build_files/50-ublue.sh
@@ -8,10 +8,7 @@ dnf5 -y copr enable ublue-os/packages
 dnf5 -y copr enable ublue-os/staging
 
 dnf5 -y install ublue-os-media-automount-udev
-dnf5 -y install ublue-os-update-services
 dnf5 -y install ublue-brew
-
-dnf5 -y remove toolbox
 
 systemctl --global enable podman.socket || true
 systemctl --global enable podman-auto-update.timer || true
